48d3cab7525319783b10b2c1247c998723ff6118
AtomCode01 <atomcode2002@gmail.com>
2024-11-18 13:43:38 +0530

    Hyperperameter tunning

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,34 +1,35 @@
 import sys
-import os
-import pandas as pd
-import numpy as np
-
 from dataclasses import dataclass
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.impute import SimpleImputer
+
+import numpy as np 
+import pandas as pd
 from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder,StandardScaler
 
 from src.exception import CustomException
 from src.logger import logging
+import os
+
 from src.utils import save_object
 
 @dataclass
-class DataTransfromationConfig:
-    preprocessor_obj_file_path= os.path.join('artifacts','preprocessor.pkl')
+class DataTransformationConfig:
+    preprocessor_obj_file_path=os.path.join('artifacts',"proprocessor.pkl")
 
 class DataTransformation:
     def __init__(self):
-        # making connection to DataTransformationConfig
-        self.data_transformation_config = DataTransfromationConfig() 
+        self.data_transformation_config=DataTransformationConfig()
 
-    def data_transformation_object(self):
-        """
-        This function transform numerical and categorical data 
-        """
+    def get_data_transformer_object(self):
+        '''
+        This function si responsible for data trnasformation
+        
+        '''
         try:
-            numerical_columns= ["writing_score","reading_score"]
-            categorical_columns= [
+            numerical_columns = ["writing_score", "reading_score"]
+            categorical_columns = [
                 "gender",
                 "race_ethnicity",
                 "parental_level_of_education",
@@ -36,77 +37,88 @@ class DataTransformation:
                 "test_preparation_course",
             ]
 
-            # creating num pipeline
-            num_pipeline = Pipeline(
+            num_pipeline= Pipeline(
                 steps=[
-                    ("impute",SimpleImputer(strategy="median")),
-                    ("scaler", StandardScaler())
-                ]
-            )
-            # creating cat_pipeline
-            cat_pipeline = Pipeline(
-                steps=[
-                    ("impute",SimpleImputer(strategy="most_frequent" )),
-                    ("one_hot_encoder",OneHotEncoder()),
-                    ("scaler",StandardScaler())
-                ]
-            )
-            logging.info(f"Numerical columns {numerical_columns}")
-            logging.info(f"Categorical columns {categorical_columns}")
+                ("imputer",SimpleImputer(strategy="median")),
+                ("scaler",StandardScaler())
 
-            # combining both pipelines
-            preprocessor = ColumnTransformer(
-                [
-                    ("num_pipeline",num_pipeline, numerical_columns),
-                    ("cat_pipeline",cat_pipeline, categorical_columns)
                 ]
+            )
+
+            cat_pipeline=Pipeline(
+
+                steps=[
+                ("imputer",SimpleImputer(strategy="most_frequent")),
+                ("one_hot_encoder",OneHotEncoder()),
+                ("scaler",StandardScaler(with_mean=False))
+                ]
+
+            )
+
+            logging.info(f"Categorical columns: {categorical_columns}")
+            logging.info(f"Numerical columns: {numerical_columns}")
+
+            preprocessor=ColumnTransformer(
+                [
+                ("num_pipeline",num_pipeline,numerical_columns),
+                ("cat_pipelines",cat_pipeline,categorical_columns)
+
+                ]
+
+
             )
 
             return preprocessor
-
-        except Exception as e:
-            raise CustomException(e, sys)
         
-    def initiate_data_transfromation(self, train_path, test_path):
-        try:
-            train_df= pd.read_csv(train_path)
-            test_df = pd.read_csv(test_path)
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+    def initiate_data_transformation(self,train_path,test_path):
 
-            logging.info("Reading train and test data completed")
+        try:
+            train_df=pd.read_csv(train_path)
+            test_df=pd.read_csv(test_path)
+
+            logging.info("Read train and test data completed")
 
             logging.info("Obtaining preprocessing object")
 
-            preprocessing_obj = self.data_transformation_object()
+            preprocessing_obj=self.get_data_transformer_object()
 
-            target_column_name = "math_score"
-            
-            # dropping target column form train data
-            input_feature_train_df= train_df.drop(columns=[target_column_name], axis=1) # = X_train
-            target_feature_train_df = train_df[target_column_name] # = y_train
+            target_column_name="math_score"
+            numerical_columns = ["writing_score", "reading_score"]
 
-            input_feature_test_df= test_df.drop(columns=[target_column_name], axis=1) # = X_test
-            target_feature_test_df = test_df[target_column_name] # = y_test
+            input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
+            target_feature_train_df=train_df[target_column_name]
 
+            input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
+            target_feature_test_df=test_df[target_column_name]
 
-            logging.info("Appying preprocessing obj on training dataset and testing dataset")
+            logging.info(
+                f"Applying preprocessing object on training dataframe and testing dataframe."
+            )
 
-            input_feature_train_arr= preprocessing_obj.fit_transform(input_feature_train_df)
-            input_feature_test_arr= preprocessing_obj.transform(input_feature_test_df)
+            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
 
-            train_arr = np.c_[input_feature_train_arr, np.array(input_feature_train_df)]
-            test_arr = np.c_[input_feature_test_arr, np.array(input_feature_test_df)]
+            train_arr = np.c_[
+                input_feature_train_arr, np.array(target_feature_train_df)
+            ]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
 
+            logging.info(f"Saved preprocessing object.")
 
-            # this code saves obj in file path
-            save_object (
-                file_path= self.data_transformation_config.preprocessor_obj_file_path, # model path
-                obj= preprocessing_obj # model
+            save_object(
+
+                file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                obj=preprocessing_obj
+
             )
 
             return (
                 train_arr,
                 test_arr,
-                self.data_transformation_config.preprocessor_obj_file_path, #pickle file path
+                self.data_transformation_config.preprocessor_obj_file_path,
             )
         except Exception as e:
-            raise CustomException(e, sys)
+            raise CustomException(e,sys)
--- src/exception.py
+++ src/exception.py
@@ -1,18 +1,24 @@
-import sys # it gets variable info before hand by interpreter which we can use to show error.
+import sys
+from src.logger import logging
 
-def error_info_detail(error, error_detail:sys):
-    _,_,exc_tb=error_detail.exc_info() # we not need first two info
-    file_name=exc_tb.tb_frame.f_code.co_filename # in exception docs
-    error_message= "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
-        file_name, exc_tb.tb_lineno, str(error)
+def error_message_detail(error,error_detail:sys):
+    _,_,exc_tb=error_detail.exc_info()
+    file_name=exc_tb.tb_frame.f_code.co_filename
+    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
+     file_name,exc_tb.tb_lineno,str(error))
 
-    )
     return error_message
 
-class CustomException:
-    def __init___(self, error_message, error_detail:sys):
-        super().__init__(error_message)
-        self.error_message=error_info_detail(error_message, error_detail=error_detail)
+    
 
+class CustomException(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message=error_message_detail(error_message,error_detail=error_detail)
+    
     def __str__(self):
         return self.error_message
+    
+
+
+        
